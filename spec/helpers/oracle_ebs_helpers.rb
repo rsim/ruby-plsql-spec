d6a5271b27e47a7f3a7e4e1b4bb8bfc6cf073ab0
Raimonds Simanovskis <raimonds.simanovskis@gmail.com>
2010-01-20 10:29:30 +0200

    Oracle E-Business Suite session initialization example

---
+++ spec/helpers/oracle_ebs_helpers.rb
@@ -0,0 +1,31 @@
+def init_ebs_user(params)
+  # replace with default user name and responsibility and then it will not be necessary
+  # to specify default value as parameter
+  params = {
+      :user_name => "default user name",
+      :responsibility_name => "default responsibility"
+     }.merge(params)
+
+  row = plsql.select(:first, "SELECT usr.user_name, res.responsibility_name, usr.user_id,
+                       urg.responsibility_id, urg.responsibility_application_id resp_appl_id
+                  FROM apps.fnd_user_resp_groups urg,
+                       applsys.fnd_user usr,
+                       fnd_responsibility_vl res
+                 WHERE usr.user_name = :user_name
+                   AND res.responsibility_name = :responsibility_name
+                   AND urg.user_id = usr.user_id
+                   AND res.responsibility_id = urg.responsibility_id",
+                   params[:user_name], params[:responsibility_name])
+
+  raise ArgumentError, "Wrong user name or responsibility name" unless row
+
+  plsql.fnd_global.apps_initialize(
+        :user_id => row[:user_id], 
+        :resp_id => row[:responsibility_id], 
+        :resp_appl_id => row[:resp_appl_id]
+        )
+
+  # uncomment if logging to dbms_output is necessary
+  # plsql.dbms_output.put_line("Initialized " + params[:user_name] + " / " + params[:responsibility_name])
+
+end
---
+++ spec/oracle_ebs_spec.rb
@@ -0,0 +1,46 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+describe "Oracle E-Business Suite" do
+  before(:all) do
+    plsql.connection = OCI8.new "APPS", "APPS", "VIS"
+    @user_name = "OPERATIONS"
+    @responsibility_name = "System Administrator"
+  end
+
+  describe "Session initialization" do
+    it "should initialize session with valid user and responsibility" do
+      lambda {
+        init_ebs_user(:user_name => @user_name, :responsibility_name => @responsibility_name)
+      }.should_not raise_error
+    end
+
+    it "should raise error with invalid user" do
+      lambda {
+        init_ebs_user(:user_name => "INVALID", :responsibility_name => @responsibility_name)
+      }.should raise_error(/Wrong user name or responsibility name/)
+    end
+
+    it "should raise error with invalid responsibility" do
+      lambda {
+        init_ebs_user(:user_name => @user_name, :responsibility_name => "INVALID")
+      }.should raise_error(/Wrong user name or responsibility name/)
+    end
+
+  end
+
+  describe "Session information" do
+    before(:all) do
+      init_ebs_user(:user_name => @user_name, :responsibility_name => @responsibility_name)
+    end
+
+    it "should return user name" do
+      plsql.fnd_global.user_name.should == @user_name
+    end
+
+    it "should return responsibility name" do
+      plsql.fnd_global.resp_name.should == @responsibility_name
+    end
+
+  end
+
+end
